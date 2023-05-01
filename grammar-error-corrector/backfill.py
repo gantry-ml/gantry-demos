e79b5b6cc77e84940fdadcf3499ed503df59849d
Sameena <sameena@gantry.io>
2023-05-01 11:55:08 -0700

    moved GEC to new SDK

--- grammar-error-corrector/backfill.py
+++ grammar-error-corrector/backfill.py
@@ -4,10 +4,11 @@ import datetime
 
 from config import logger, GantryConfig, DataStorageConfig
 import gantry
-import gantry.query as gantry_query
+from gantry.query.time_window import TimeWindow
 
 gantry.init(api_key=GantryConfig.GANTRY_API_KEY, send_in_background=False)
-
+# application = gantry.create_application(GantryConfig.GANTRY_APP_NAME)
+application = gantry.get_application(GantryConfig.GANTRY_APP_NAME)
 
 def retrieve_data() -> pd.DataFrame:
     logger.info("Retrieving demo data from public S3 bucket")
@@ -34,8 +35,7 @@ def load_to_gantry(df: pd.DataFrame, gantry_env: str):
             errors="coerce"),
         )
 
-    gantry.log_records(
-        application=GantryConfig.GANTRY_APP_NAME,
+    application.log(
         timestamps=[ts.to_pydatetime() for ts in datetime],
         inputs=df[["text", "account_age_days"]],
         row_tags=df[["username"]].to_dict("records"),
@@ -80,61 +80,54 @@ LAST_WEEK_END = _utc_time_helper(
 )
 THIS_WEEK_START = LAST_WEEK_END
 THIS_WEEK_END = _utc_time_helper(2022, 4, 26)
-THIS_WEEK_VIEW, LAST_WEEK_VIEW = "this-week", "last-week"
-ACCEPTED_VIEW, REJECTED_VIEW = "accepted", "rejected"
+THIS_WEEK_QUERY, LAST_WEEK_QUERY = "this-week", "last-week"
+ACCEPTED_QUERY, REJECTED_QUERY = "accepted", "rejected"
 
 
-def create_views(application_name: str):
-    # This week view
-    logger.info(f"Creating view named {THIS_WEEK_VIEW} for {application_name}")
-    gantry_query.create_view(
-        application=application_name,
-        name=THIS_WEEK_VIEW,
-        start_time=THIS_WEEK_START,
-        end_time=THIS_WEEK_END,
-        data_filters=_env_filter_helper(),
+def create_queries():
+    # This week query
+    logger.info(f"Creating query named {THIS_WEEK_QUERY} for {application._name}")
+    query = application.query(
+        time_window=TimeWindow(start_time=THIS_WEEK_START, end_time=THIS_WEEK_END),
+        filters=_env_filter_helper(),
     )
-    # Last week view
-    logger.info(f"Creating view named {LAST_WEEK_VIEW} for {application_name}")
-    gantry_query.create_view(
-        application=application_name,
-        name=LAST_WEEK_VIEW,
-        start_time=LAST_WEEK_START,
-        end_time=LAST_WEEK_END,
-        data_filters=_env_filter_helper(),
-    )
+    application.save_query(THIS_WEEK_QUERY, query)
 
-    # Accepted view
-    logger.info(f"Creating view named {ACCEPTED_VIEW} for {application_name}")
-    gantry_query.create_view(
-        application=application_name,
-        name=ACCEPTED_VIEW,
-        start_time=THIS_WEEK_START,
-        end_time=THIS_WEEK_END,
-        data_filters=_env_filter_helper() + _correction_filter_helper(True),
+    # Last week query
+    logger.info(f"Creating query named {LAST_WEEK_QUERY} for {application._name}")
+    query = application.query(
+        time_window=TimeWindow(start_time=LAST_WEEK_START, end_time=LAST_WEEK_END),
+        filters=_env_filter_helper(),
     )
+    application.save_query(LAST_WEEK_QUERY, query)
 
-    # Rejected view
-    logger.info(f"Creating view named {REJECTED_VIEW} for {application_name}")
-    gantry_query.create_view(
-        application=application_name,
-        name=REJECTED_VIEW,
-        start_time=THIS_WEEK_START,
-        end_time=THIS_WEEK_END,
-        data_filters=_env_filter_helper() + _correction_filter_helper(True),
+    # Accepted query
+    logger.info(f"Creating query named {ACCEPTED_QUERY} for {application._name}")
+    query = application.query(
+        time_window=TimeWindow(start_time=THIS_WEEK_START, end_time=THIS_WEEK_END),
+        filters=_env_filter_helper() + _correction_filter_helper(True),
     )
+    application.save_query(ACCEPTED_QUERY, query)
+
+    # Rejected query
+    logger.info(f"Creating query named {REJECTED_QUERY} for {application._name}")
+    query = application.query(
+        time_window=TimeWindow(start_time=THIS_WEEK_START, end_time=THIS_WEEK_END),
+        filters=_env_filter_helper() + _correction_filter_helper(True),
+    )
+    application.save_query(REJECTED_QUERY, query)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--load-data", action="store_true")
-    parser.add_argument("--create-views", action="store_true")
+    parser.add_argument("--create-queries", action="store_true")
     args = parser.parse_args()
-    if not (args.load_data or args.create_views):
-        logger.error("Must pass either '--load-data' or '--create-views'")
+    if not (args.load_data or args.create_queries):
+        logger.error("Must pass either '--load-data' or '--create-queries'")
     else:
         if args.load_data:
             data = retrieve_data()
             load_to_gantry(data, GantryConfig.GANTRY_PROD_ENV)
-        if args.create_views:
-            create_views(GantryConfig.GANTRY_APP_NAME)
+        if args.create_queries:
+            create_queries()
